8317ef0ac92805a87b4f6eadc0c60d7f33352ece
jarora-hoopp <42554788+jarora-hoopp@users.noreply.github.com>
2019-07-24 16:24:24 -0400

    Updated ForceNewSSLCert

--- Ansible/Windows/ansible_prep.ps1
+++ Ansible/Windows/ansible_prep.ps1
@@ -1,14 +1,14 @@
 ﻿#This script is a configuration script for Packer to call on to download the files required for Ansible WinRM creation. Sysprep will run the ConfigureRemotingForAnsible script using the unattend.xml downloaded in this script.
 
 $url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
-$file = "C:\Temp\ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
           
-$url2 = "https://raw.githubusercontent.com/jarora-hoopp/PublicRepo/master/Ansible/Windows/unattend.xml"
-$file2 = "C:\Temp\unattend.xml"
+#$url2 = "https://raw.githubusercontent.com/jarora-hoopp/PublicRepo/master/Ansible/Windows/unattend.xml"
+#$file2 = "$env:temp\unattend.xml"
 
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
-(New-Object -TypeName System.Net.WebClient).DownloadFile($url2, $file2)
+#(New-Object -TypeName System.Net.WebClient).DownloadFile($url2, $file2)
 
-#powershell.exe -ExecutionPolicy ByPass -File $file
+powershell.exe -ExecutionPolicy ByPass -File $file -ForceNewSSLCert
 #winrm enumerate winrm/config/Listener
 
