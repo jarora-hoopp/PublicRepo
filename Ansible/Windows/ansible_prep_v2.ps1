6794a03899b3a95add0a0c2929b05b6cfb32be62
jarora-hoopp <42554788+jarora-hoopp@users.noreply.github.com>
2019-07-26 14:18:26 -0400

    Update ansible_prep_v2.ps1

--- Ansible/Windows/ansible_prep_v2.ps1
+++ Ansible/Windows/ansible_prep_v2.ps1
@@ -1,6 +1,6 @@
 ﻿#This script is a configuration script for Packer to call on to download the files required for Ansible WinRM creation. Sysprep will run the ConfigureRemotingForAnsible script using the unattend.xml downloaded in this script.
           
-$url = "https://raw.githubusercontent.com/jarora-hoopp/PublicRepo/master/Ansible/Windows/unattend.xml"
+$url = "https://raw.githubusercontent.com/jarora-hoopp/PublicRepo/master/Ansible/Windows/unattend1.xml"
 $file = "$env:SystemRoot\system32\sysprep\unattend.xml"
 
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
